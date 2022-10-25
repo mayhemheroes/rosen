bf12322831a0fc8bab8247518ddf0f17e2adad95
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-24 23:21:03 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,48 @@
+package fuzz
+
+import "strconv"
+import "github.com/awnumar/rosen/config"
+import "github.com/awnumar/rosen/crypto"
+
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) > 1 {
+        num, _ = strconv.Atoi(string(bytes[0]))
+
+        switch num {
+    
+        case 0:
+            content := string(bytes)
+            config.DecodeKeyString(content)
+            return 0
+
+        case 1:
+            content := string(bytes)
+            config.LoadConfig(content)
+            return 0
+
+        case 2:
+            var test crypto.Cipher
+            test.Encrypt(bytes)
+            return 0
+
+        case 3:
+            var test crypto.Cipher
+            test.Decrypt(bytes)
+            return 0
+
+        default:
+            crypto.NewCipher(bytes)
+            return 0
+
+        }
+    }
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
